f773510e8adb50bafd402fbc276e980592f8e181
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-10 20:10:39 -0300

    configure eslint and babel

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+export default {
+    "env": {
+        "browser": true
+    },
+    "plugins":["react"],
+    "extends":[
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser":"babel-eslint"
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
-const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HotModuleReplacementPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  output: {
+    path:__dirname +'/dir',
+    filename: 'bundle.js',
+  },
+  "devtool":"source-map",
   module: {
     rules: [
       {
+        "enforce": "pre",
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
+        loader: 'eslint-loader',
+        options: {
+          "emitWarning":true,
+          "failOnError": false,
+          "failOnWarning": false
+        },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use:{
+          "loader":'style-loader'
+        },
       },
+      {
+        test:/\.css$/,
+        use:[
+          "style-loader",'css-loader'
+        ],
+      }
     ],
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public/'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true,
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [new HtmlWebpackPlugin({
+    template:"./src/index.html",
+    filename: "./index.html"
+    })
+  ],
 };
