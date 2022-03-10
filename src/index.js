1999669efa60029c9dc6a3099d529a6c9a627b6d
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-09 22:58:12 -0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './App';
 
 //find our app div defined in index.html and render the app inside that
 ReactDOM.render(<App />, document.getElementById('root'));
